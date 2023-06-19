3509e1f667cc157a7d757e39f1182cc69128f91f
Lucas Miguel <lucasvieira.dev@gmail.com>
2023-06-19 22:40:21 +0200

    fix test 2

---
+++ test/unit/utils/validation.util.test.ts
@@ -0,0 +1,26 @@
+import { validate } from "@/utils/validation.util";
+import * as yup from "yup";
+
+describe("validate", () => {
+  it("should return an object with result", async () => {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+    });
+
+    const { result, error } = await validate(schema, { name: "John" });
+
+    expect(result).toEqual({ name: "John" });
+    expect(error).toBeNull();
+  });
+
+  it("should return an object with error", async () => {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+    });
+
+    const { result, error } = await validate(schema, {});
+
+    expect(result).toBeNull();
+    expect(error).toBeInstanceOf(yup.ValidationError);
+  });
+});
